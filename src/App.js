1974f379db7c7f1a80d8e3e778b2053085c68abc
Se-pher <hunglqgch210012@fpt.edu.vn>
2024-01-15 15:21:51 +0700

    Card item

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // App.js
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import Header from './components/Header';
-import ProductList from './components/ProductList';
-import Checkout from './components/Checkout';
-
+import Cart from './components/Cart';
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Header />
-        <ProductList />
-        <Checkout />
-      </div>
+      <Router>
+        <div>
+          {/* <Header /> */}
+          <Cart />
+        </div>
+      </Router>
     </Provider>
   );
 }
